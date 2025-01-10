3cb3cc8409678652d387350134b9702742e075f4
Leonardo Moreira Guidorizzi <leonardo.tesoledy@gmail.com>
2025-01-10 10:14:45 -0300

    exploring ways to do an constructor

--- aulas/AdvancedJS/ObjectAndPrototypes/index50.js
+++ aulas/AdvancedJS/ObjectAndPrototypes/index50.js
@@ -25,3 +25,57 @@ function Product(name, price, stock) {
 const p1 = new Product("Camiseta", 15, 3);
 p1.privateStock = 5;
 console.log(p1.stock);
+function Person(first, last, age, eye) {
+  this.first = first;
+  this.last = last;
+  this.age = age;
+  this.eye = eye;
+}
+const p2 = new Person("Leonardo", "Guidorizzi", 19, "brown");
+console.log(p2);
+delete p2.first;
+console.log(p2);
+let animal = {
+  "likes to play": true,
+  tailColor: "brown",
+  size: "2m",
+};
+console.log(animal);
+// const Persona = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   language: "",
+//   set lang(lang) {
+//     this.language = lang;
+//   },
+// };
+// Persona.lang = "Pt"; // eu tenho que usar atribuindo > atraves do set eu estou atribuindo o valor na language
+// console.log(Persona);
+
+console.log(animal);
+function Persona(firstName = "", lastName = "") {
+  //outra maneira de fazer um contrutor
+  let _firstName = firstName;
+  let _lastName = lastName;
+  Object.defineProperty(this, "firstName", {
+    // eu passo o objeto e o atributo
+    set: function (value) {
+      this._firstName = value;
+    },
+    get: function () {
+      return _firstName;
+    },
+  });
+  Object.defineProperty(this, "lastName", {
+    set: function (value) {
+      this.lastName = value;
+    },
+    get: function () {
+      return _lastName;
+    },
+  });
+}
+const persona1 = new Persona();
+persona1.firstName = "Leonardo";
+persona1.lastName = "Moreira";
+console.log(persona1);
